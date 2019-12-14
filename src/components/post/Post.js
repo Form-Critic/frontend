9e95080ab1e54c9dbc0fd86b0290b23ee4ecbb7b
Uzias Rivera <uziasrivera@gmail.com>
2019-12-14 14:10:29 -0800

    styling post page

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,26 +1,56 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import AppBar from '@material-ui/core/AppBar';
 import { makeStyles } from '@material-ui/core/styles';
 import styled from 'styled-components'
-import {useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getPost, getComments, getCurrentUser } from '../../actions/index'
 import Comments from './Comments'
 
-const useStyles = makeStyles(theme=>({
-    root:{
-        margin: '5% auto',
-        alignSelf:'center',
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        alignContent:'center',
-        width: '100%'
+const useStyles = makeStyles(theme => ({
+
+    root: {
+        margin: '2% auto',
+        width: '80%',
     },
-    comment:{
-        display:'none',
-        color:'red'
-    }
+    content: {
+        margin: '5% auto',
+        alignSelf: 'center',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        alignContent: 'center',
+        width: '90%'
+    },
+    comment: {
+        display: 'none',
+        color: 'red'
+    },
+    description: {
+        width: '50%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    title: {
+        margin: '2% auto 0 auto'
+    },
+    subTitle:{
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'space-between',
+        width: '30%',
+        margin: '0 auto'
+    },
 })
 )
 
@@ -28,35 +58,59 @@ const useStyles = makeStyles(theme=>({
 
 
 const Post = (props) => {
-  const classes = useStyles();  
-  const dispatch = useDispatch()
-  const currentState = useSelector(state=>state)
-  useEffect(()=>{
+    const classes = useStyles();
+    const dispatch = useDispatch()
+    const post = useSelector(state => state.post)
+    useEffect(() => {
         dispatch(getPost(props.match))
         dispatch(getComments(props.match))
-    },[])
-    const embedLink = !(currentState.post.video_link===undefined)?'//www.youtube.com/embed/'+currentState.post.video_link.split('=')[1]:"";
-    const x = new Date() 
+    }, [])
+    const embedLink = !(post.video_link === undefined) ? '//www.youtube.com/embed/' + post.video_link.split('=')[1] : "";
+    const x = new Date()
 
-    
-    return(currentState.post.comments===undefined)?<h1>Loading</h1>:(
+
+    return (post.comments === undefined) ? <h1>Loading</h1> : (
         <div>
-            {console.log(x)}
-            <iframe
-            title='video'
-            width="560"
-            height="315"
-            src={embedLink}
-            allowFullScreen
-            frameBorder="0"
-            >
-            </iframe>
+            <CssBaseline />
+            <AppBar position="relative">
+                <Toolbar>
+                    <CameraIcon className={classes.icon} />
+                    <Typography variant="h6" color="inherit" noWrap> Album layout </Typography>
+                </Toolbar>
+            </AppBar>
+            <div className={classes.title}>
+                <Typography variant='h2'>{post.title}</Typography>
+                <div className={classes.subTitle}>
+                    <Typography style={{fontSize:'16px'}}variant='subtitle1'>{post.date}</Typography>
+                    <Typography style={{fontSize:'16px'}}variant='subtitle1'>{post.username}</Typography>
+                </div>
+            </div>
             <div className={classes.root}>
-                <Comments className={classes.comment} comments={currentState.post.comments} props={props}></Comments>
+                <iframe
+                    title='video'
+                    width="560"
+                    height="315"
+                    src={embedLink}
+                    allowFullScreen
+                    frameBorder="0"
+                >
+                </iframe>
+            </div>
+            <div>
+                <Comments className={classes.comment} comments={post.comments} props={props}></Comments>
             </div>
         </div>
     );
 };
 
 export default Post;
+
+{/* <div className={classes.description}>
+                    <Paper className={classes.title}>
+                        <Typography variant='h4'>{post.title}</Typography>
+                        {/* <CardMedia  component="img" style={{width:'12%'}} image={post.avatar} src={post.avatar}></CardMedia> */}
+//     <img src={post.avatar} style={{width:'54px', height:'100%'}}/>
+// </Paper>
+// <Typography>{post.description}</Typography> */}
+{/* </div> */ }
 //<iframe width="560" height="315" src="//www.youtube.com/embed/SHgQeBk7zIs" frameborder="0" allowfullscreen></iframe>
