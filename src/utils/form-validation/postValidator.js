9a95e5c363cec196c7d2c270ef8bddf37ab5b73d
Uzias Rivera <uziasrivera@gmail.com>
2020-04-17 15:43:11 -0700

    updated email regex

--- src/utils/form-validation/postValidator.js
+++ src/utils/form-validation/postValidator.js
@@ -1,32 +1,8 @@
-// export default function validate(values) {
-//     const ytRegex = RegExp('^(http(s)??\\:\\/\\/)?(www\\.)?((youtube\\.com\\/watch\\?v=)|(youtu.be\\/))([a-zA-Z0-9\\-_])+$')
-//     let errors = {};
-//     if (!values.title) {
-//         errors.title = 'Title is required';
-//     }
-//     if (!values.video_link) {
-//         errors.video_link = 'YouTube link is required';
-
-//     }
-//     else if (!ytRegex.test(values.video_link)) {
-//         errors.video_link = 'At the moment YouTube links are only supported. Please provide valid link.'
-//     }
-//     if(!values.description){
-//         errors.description = 'Description is required.'
-//     }
-//     else if (values.description.split(/\s+/).length<10){
-
-//         errors.description = 'Description should be meaningful, please write at least 10 words.'
-//     }
-
-
-//     return errors;
-// };
 
 export default function validate(values, formType="post") {
 
     const ytRegex = RegExp('^(http(s)??\\:\\/\\/)?(www\\.)?((youtube\\.com\\/watch\\?v=)|(youtu.be\\/))([a-zA-Z0-9\\-_])+$')
-    const emailRegex = RegExp("^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$")
+    const emailRegex = RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
 
     let errors = {};
     switch(formType){
